9a77b3931460afb802e7b6488b946751830b5093
Jonny Lawless <JonnyLawless78@gmail.com>
2022-04-01 21:02:14 -0500

    Update wikiSearch.php

--- wikiSearch.php
+++ wikiSearch.php
@@ -1,4 +1,5 @@
 <doctype html>
+<?include_once $_SERVER['DOCUMENT_ROOT'] . "/../php/wikiDBConfig.php" ?>
 <html>
 	<head>
 		<title>Search for a crystal!</title>
@@ -6,7 +7,7 @@
 	<body>
 		<form action="<?echo $_SERVER['REQUEST_URI'];?>">
 			<div class="autocomplete" style="width: 300px;">
-				<input id="searchBar" type="text" name="myCountry" placeholder="Crystal Name">
+				<input id="searchBar" type="text" name="crystal" placeholder="Crystal Name">
 			</div>
 			<input type="submit">
 		</form>
