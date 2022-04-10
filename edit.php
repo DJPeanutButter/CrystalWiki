32431cade069edde3223948c84b6c1edcfd54081
Jonny Lawless <JonnyLawless78@gmail.com>
2022-04-10 16:46:15 -0500

    Update edit.php

--- edit.php
+++ edit.php
@@ -18,7 +18,7 @@
                                             ($_POST ["chakra5"] === "on"),
                                             ($_POST ["chakra6"] === "on"),
                                             ($_POST ["chakra7"] === "on")));
-        echo "!";
+        echo $wikiChakras;
             $wikiPlanets = wikiEncode(array(($_POST ["plnSaturn"] === "on"),
                                             ($_POST ["plnJupiter"] === "on"),
                                             ($_POST ["plnMars"] === "on"),
