f2dcebef0c414be9f186536d20bb086351ab61ff
Jonny Lawless <JonnyLawless78@gmail.com>
2022-04-10 16:39:47 -0500

    Update edit.php

--- edit.php
+++ edit.php
@@ -10,6 +10,70 @@
       if (isset($_POST["name"])){
         $postData = $_POST;
         echo var_dump($postData);
+        
+            //Generate strings for $wikiChakras, $wikiPlanets, $wikiElements & $wikiZodiac
+            $wikiChakras = wikiEncode(array(($_POST ["chakra1"] === "on"),
+                                            ($_POST ["chakra2"] === "on"),
+                                            ($_POST ["chakra3"] === "on"),
+                                            ($_POST ["chakra4"] === "on"),
+                                            ($_POST ["chakra5"] === "on"),
+                                            ($_POST ["chakra6"] === "on"),
+                                            ($_POST ["chakra7"] === "on")));
+            
+            $wikiPlanets = wikiEncode(array(($_POST ["plnSaturn"] === "on"),
+                                            ($_POST ["plnJupiter"] === "on"),
+                                            ($_POST ["plnMars"] === "on"),
+                                            ($_POST ["plnVenus"] === "on"),
+                                            ($_POST ["plnMercury"] === "on"),
+                                            ($_POST ["plnMoon"] === "on"),
+                                            ($_POST ["plnSun"] === "on")));
+            
+            $wikiElements = wikiEncode(array(($_POST ["eleAir"] === "on"),
+                                             ($_POST ["eleWater"] === "on"),
+                                             ($_POST ["eleEarth"] === "on"),
+                                             ($_POST ["eleFire"] === "on")));
+            
+            $wikiZodiac = wikiEncode (array(($_POST ["zodAries"] === "on"),
+                                            ($_POST ["zodTaurus"] === "on"),
+                                            ($_POST ["zodGemini"] === "on"),
+                                            ($_POST ["zodCancer"] === "on"),
+                                            ($_POST ["zodLeo"] === "on"),
+                                            ($_POST ["zodVirgo"] === "on"),
+                                            ($_POST ["zodLibra"] === "on"),
+                                            ($_POST ["zodScorpio"] === "on"),
+                                            ($_POST ["zodSagittarius"] === "on"),
+                                            ($_POST ["zodCapricorn"] === "on"),
+                                            ($_POST ["zodAquarius"] === "on"),
+                                            ($_POST ["zodPisces"] === "on")));
+            
+            
+          try {
+    				$conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+            
+            // set the PDO error mode to exception
+            $query = $conn->prepare ("update Crystals set Description=:des, Luster=:lst, Color=:clr, Streak=:str, Hardness=:hrd, SpecificGravity=:spg, Chakras=:ckr, ConsonantCrystals=:con, DissonantCrystals=:dis, Planets=:pln, Elements=:ele, Zodiac=:zod where Name=:nam
+            $query->bindParam (':nam', $_POST["name"]);
+            $query->bindParam (':des', $_POST["description"]);
+            $query->bindParam (':lst', $_POST["luster"]);
+            $query->bindParam (':clr', $_POST["color"]);
+            $query->bindParam (':str', $_POST["streak"]);
+            $query->bindParam (':hrd', $_POST["hardness"]);
+            $query->bindParam (':spg', $_POST["specificGravity"]);
+            $query->bindParam (':ckr', $wikiChakras);
+            $query->bindParam (':con', $_POST ["consonants"]);
+            $query->bindParam (':dis', $_POST ["dissonants"]);
+            $query->bindParam (':pln', $wikiPlanets);
+            $query->bindParam (':ele', $wikiElements);
+            $query->bindParam (':zod', $wikiZodiac);
+            
+            $query->execute ();
+            
+            echo "<script>var message=\"Record succesfully created for ".$_POST["name"]."\";</script>";
+            
+          }catch(PDOException $e){
+            echo "Boo<br>Error: " . $e->getMessage();
+            echo "<script>var message=\"$e->getMessage()\"</script>";
+          }
       }
       
       if (isset($_GET["crystal"])){
